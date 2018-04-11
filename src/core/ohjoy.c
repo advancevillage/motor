e48e98aa85a15b319759a20f71d76be22144ac34
advancevillage <cugriver@gmail.com>
2018-04-11 23:57:48 +0800

    auto compelete pass

--- src/core/ohjoy.c
+++ src/core/ohjoy.c
@@ -1,3 +1,13 @@
 /*
  * Copyright (C) Richard Sun
  */
+#include <stdio.h>
+#include "ohjoy.h" 
+
+int main(){
+    struct Student s;
+    s.id = 10;
+    printf("hello ohjoy \n");
+    printf("Student ID is %d\n", s.id);
+    return 0;
+}
---
+++ src/core/ohjoy.h
@@ -0,0 +1,8 @@
+#ifndef __OHJOY__H__
+#define __OHJOY__H__
+
+struct Student {
+    int id;
+};
+
+#endif
